affad6656db6184226f9c28d5545daf016c9785f
Annanya-Tayal <147336630+Annanya-Tayal@users.noreply.github.com>
2023-11-03 21:21:04 +0530

    File added successfully

---
+++ C++_course/week2/exercise4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+int seven = 7;
+int two = 2;
+cout << ( (double) seven / (double) two ) << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
