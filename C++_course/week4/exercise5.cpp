0df276edc7f56c230af92bfaaafa3bc4754c9527
Annanya-Tayal <147336630+Annanya-Tayal@users.noreply.github.com>
2023-11-03 21:34:53 +0530

    File added successfully

---
+++ C++_course/week4/exercise5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+  for(int i = 0 ; i <=4 ; i ++)
+  {
+    for(int j = 4-i; j>0; j--)
+    {
+      cout<<".";
+    }
+    cout<<i+1<<endl;
+  }
+
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
